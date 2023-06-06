c61e9e07152dc042626ad6fd9665fd6f43b63646
Jonathan Tremblay <jtrembla@cstj.qc.ca>
2023-06-06 14:43:10 -0400

    Version 0.9.8 - First public version

---
+++ ReadMe.md
@@ -0,0 +1,65 @@
+# UNITY LIBRARIES CLEANER
+
+This is a Batch Script to free up disk space by emptying the Library folders of your Unity projects.
+(Don't worry, when you open a project, Unity can always rebuild the content of the Library folder.)
+
+## Table of Contents
+
+1. [Getting Started](#getting-started)
+2. [Compatibility](#compatibility)
+3. [Known Issues](#known-issues)
+4. [About the Project](#about-the-project)
+5. [Contact](#contact)
+6. [Version History](#version-history)
+7. [License](#license)
+8. [Legal](#legal)
+
+## Getting Started
+
+ Usage and warnings:
+ * The .bat file must be placed in a folder containing one or more Unity projects (make sure you have write access for this folder).
+ * Double-click on the UnityLibrariesCleaner.bat script to make it run.
+ * If Windows prevents the script from running, click on "More info" and then "Run anyway" (don't take my word for it, take a look at the code first!)
+ * The script finds all valid Library folders and then offers Manual or Automatic mode.
+ * Then the contents of the folders are permanently deleted (not placed in the recycle bin).
+ * The "Library/LastSceneManagerSetup.txt" files are preserved (allows to keep the last open scene of each project).
+ That's it!
+
+## Compatibility
+
+* Tested on Windows.
+
+## Known Issues
+
+* If the script has a lot of folders to examine, the process may take a long time.
+
+## About the Project
+
+* I created this tool to free up some space on my drives, I hope it helps you to claim back your space to!
+
+## Contact
+
+**Jonathan Tremblay**  
+Teacher, Cegep de Saint-Jerome  
+jtrembla@cstj.qc.ca
+
+Project Repository: https://github.com/JonathanTremblay/UnityLibrariesCleaner 
+
+## Version History
+
+* 0.9.8
+    * First public version.
+
+## License
+
+This script is available for distribution and modification under the CC0 License, which allows for free use and modification.  
+https://creativecommons.org/share-your-work/public-domain/cc0/
+
+## Legal
+
+By using this script, you acknowledge and agree that:
+
+1. You are solely responsible for the usage and consequences of running this script.
+2. The author of the script and the contributors to the project are not responsible for any damages, loss of data, or other issues caused by the usage of this script.
+3. It is your responsibility to review and understand the script code before running it to ensure it meets your requirements and does not pose any security risks.
+4. Use this script at your own risk.
---
+++ UnityLibrariesCleaner.bat
@@ -0,0 +1,201 @@
+@echo OFF
+
+rem VERSION INFO
+set "Version=V0.9.8"
+set "Date=2023-06"
+rem About this version:
+rem - First public version
+
+rem Save the original encoding:
+for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
+rem Change the encoding (for accented characters):
+chcp 1252>nul
+rem Enable additional features:
+setlocal enableextensions enabledelayedexpansion
+
+rem Adjust the current directory (useful in admin context):
+cd /d "%~dp0"
+
+rem Variables for text formatting:
+set "Bold=[1m"
+set "Normal=[0m"
+set "Red=[91m"
+set "Green=[92m"
+set "RedBackground=[1;41m"
+set "RedBackgroundNormal=[41m"
+set "GreenBackground=[42;1m"
+set "Separator=************************************************************************************************************************"
+set "GreenSeparator=%GreenBackground%%Separator%%Normal%"
+set "RedSeparator=%RedBackground%%Separator%%Normal%"
+
+rem Variables for finding Library folders:
+set "SceneFile=LastSceneManagerSetup.txt"
+set "LibraryFolder=Library"
+
+echo %GreenSeparator%
+echo.
+echo %Bold% UNITY LIBRARIES CLEANER %Normal%- %Version% (%Date%)
+echo  https://github.com/JonathanTremblay/UnityLibrariesCleaner (CC0 license)
+echo.
+echo %GreenSeparator%
+echo.
+echo  This script frees up disk space by emptying the Library folders of your Unity projects.
+echo  (When you open a project, Unity can rebuild the content of the Library folder.)
+echo.
+echo  Usage and warnings:
+echo  - The .bat file must be placed in a folder that contains one or more Unity projects.
+echo  - The script first finds all valid Library folders and then offers Manual or Automatic mode.
+echo  - The folder contents are permanently deleted (not placed in the recycle bin).
+echo  - The "Library/LastSceneManagerSetup.txt" files are preserved (keep the last opened scene of the projects).
+echo.
+echo %GreenSeparator%
+echo.
+echo  Searching for folders that can be %Bold%emptied%Normal%:
+
+rem Find Library folders containing files to delete:
+set /a counter=0
+
+rem Create an array named "folders" to store the folders to process:
+set "folders="
+set "lastFolder=XYZ"
+
+for /d /r %%d in (%LibraryFolder%) do (
+	set "currentFolder=%%~d"
+	if "!currentFolder:%%~lastFolder!=!" == "!currentFolder!" (
+	   if exist "%%d\%SceneFile%" (
+		   cd %%d
+			set /a fileCount=0
+			for /r %%f in (*) do set /a fileCount+=1
+			if !fileCount! geq 2 (
+				set "lastFolder=%%~d"
+				echo  • %Red%%%~d%Normal%
+				rem Add currentFolder to the folders array, then increment the counter:
+				set "folders=!folders! "%%~d""
+				set /a counter+=1
+			)
+		)
+	)
+)
+set "foldersToDelete=%folders%"
+cd /d "%~dp0"
+echo.
+if %counter% == 0 goto NOTHING
+echo %Normal%%GreenSeparator%
+echo.
+set "plural="
+set "pronoun=this"
+if %counter% geq 2 (
+	set "plural=s"
+	set "pronoun=these"
+)
+
+:QUESTION
+echo  AVAILABLE MODES:
+echo  %Bold%[1]%Normal% - MANUAL (choose for each folder, one by one)
+echo   2  - AUTOMATIC (delete all folders)
+echo   3  - CANCEL (do not delete anything)
+echo.
+set /p mode= Which mode do you want to use to delete the contents of %pronoun% folder%plural%? (%Bold%[1]%Normal%, 2, or 3) 
+
+if "%mode%" == "2" (
+    echo   2: AUTOMATIC MODE
+    echo.
+	goto AUTOMATIC
+)
+if "%mode%" == "3" (
+    echo   3: CANCEL MODE
+    echo.
+	goto STOP
+)
+
+rem Default mode:
+echo   1: MANUAL MODE
+echo.
+goto CHOOSE
+
+:CHOOSE
+rem For loop to prompt the user for each folder:
+set "foldersToDelete="
+set /a counter=0
+for %%d in (%folders%) do (
+	set "DeleteFolder=N"
+	set /p "DeleteFolder= Delete the folder %Red%%%~d%Normal% ? (Y/%Bold%[N]%Normal%) "
+	if /i "!DeleteFolder!"=="Y" (
+		rem echo %%~d will be deleted
+		set "foldersToDelete=!foldersToDelete! %%~d"
+		set /a counter+=1
+	)
+)
+echo.
+
+set "plural="
+if %counter% geq 2 set "plural=s"
+set "number=%counter%"
+
+if %counter% geq 1 goto AUTOMATIC
+
+:NOTHING
+echo %RedSeparator%
+echo.
+echo  %Bold%NOTHING TO CLEAN ^^! %Normal%
+echo  No Library folder to empty, so the script is done.
+echo.
+echo %RedSeparator%
+goto END
+
+:STOP
+echo %RedSeparator%
+echo.
+echo  %Bold%INTERRUPTION ^^! %Normal%
+echo  Script stopped. No files deleted.
+echo.
+echo %RedSeparator%
+goto END
+
+
+:AUTOMATIC
+echo %GreenSeparator%
+echo.
+echo  %counter% deletion%plural% to perform (each deletion may take a few seconds...)
+echo.
+set /a counter = 0
+rem Clean the Library folders:
+for %%d in (%foldersToDelete%) do (
+	if exist "%%~d\%SceneFile%" (
+		cd "%%~d"
+		set /a fileCount=0
+		for /r %%f in (*) do if %%f neq "%%~d\%SceneFile%" set /a fileCount+=1
+		if !fileCount! geq 1 (
+			cd /d "%~dp0"
+			if exist "%%~d\%SceneFile%" (
+				rem Backup the last opened scene file:
+				mkdir "%%~d_Temp"
+				move "%%~d\%SceneFile%" "%%~d_Temp" >nul
+			)
+			rd /s /q "%%~d"
+			rem The following condition moves the file or renames the temp folder
+			if exist "%%~d_Temp" (
+				if exist "%%~d" (
+					move "%%~d_Temp\%SceneFile%" "%%~d" >nul
+					rd /s /q "%%~d_Temp"
+				) else ( ren "%%~d_Temp" %LibraryFolder%)
+			)
+			echo %Red% %%~d%Normal% has been emptied
+			set /a counter+=1
+		)
+	)
+)
+echo.
+echo %GreenSeparator%
+echo.
+set "plural="
+if %counter% geq 2 set "plural=S"
+echo  %Bold%SUCCESS: %counter% FOLDER%plural% EMPTIED %Normal%
+echo  Thank you for using Unity Libraries Cleaner ^^!
+echo.
+echo %GreenSeparator%
+
+:END
+echo.
+chcp %cp%>nul
+pause
